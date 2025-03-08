9032c5d551629773c52008e5e26bc0e5a8e4cfba
baneyloucks394 <baneyloucks394@gmail.com>
2025-03-08 05:20:24 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ScienceExperiments {
+    public static void main(String[] args) {
+        // Declare variables
+        int number1 = 5;
+        int number2 = 3;
+        int sum = number1 + number2;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is " + sum);
+    }
+}
