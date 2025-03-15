f82d6e4aaf658a9212c8f5387a6ab419afb16e38
baneyloucks394 <baneyloucks394@gmail.com>
2025-03-15 05:58:14 +0000

    Add 9.java

---
+++ src/9.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ScienceExperiments {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + ". Welcome to the world of science!");
+    }
+}
