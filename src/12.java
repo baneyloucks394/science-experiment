c4534664eb27ca4876f08b7224f5f8059551a8eb
baneyloucks394 <baneyloucks394@gmail.com>
2025-03-18 13:23:43 +0000

    Add 12.java

---
+++ src/12.java
@@ -0,0 +1,6 @@
+public class ExploringTheWondersOfScience {
+    public static void main(String[] args) {
+        double gravity = 9.8;
+        System.out.println("The acceleration due to gravity on Earth is " + gravity + " meters per second squared.");
+    }
+}
