939a741e423f0e172f4b4e351833c2d326ed0a7e
baneyloucks394 <baneyloucks394@gmail.com>
2025-03-19 07:05:55 +0000

    Add 13.java

---
+++ src/13.java
@@ -0,0 +1,9 @@
+public class ExploringWondersOfScience {
+    public static void main(String[] args) {
+        System.out.println("Exploring the wonders of science!");
+        int a = 10;
+        int b = 5;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
