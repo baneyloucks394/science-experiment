fb0db0194fe061412dd4c399c342aee3ae5b5067
baneyloucks394 <baneyloucks394@gmail.com>
2025-03-21 02:25:20 +0000

    Add 15.java

---
+++ src/15.java
@@ -0,0 +1,7 @@
+public class RandomJavaCodeGenerator {
+    public static void main(String[] args) {
+        // Example of generating a random integer between 1 and 100
+        int randomNumber = (int) Math.random() * 100 + 1;
+        System.out.println("Random number: " + randomNumber);
+    }
+}
