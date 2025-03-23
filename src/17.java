11b35e3098f1e42b92b4b1b4e2651a079029115a
baneyloucks394 <baneyloucks394@gmail.com>
2025-03-23 13:42:15 +0000

    Add 17.java

---
+++ src/17.java
@@ -0,0 +1,7 @@
+public class ScienceExperiment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, this is an example of a Java program.");
+        System.out.println("It's written by Qwen, created by Alibaba Cloud. You can also write your own code here!");
+    }
+}
