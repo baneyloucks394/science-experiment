128142cdd81eaf307ffc2496872171f56a173314
baneyloucks394 <baneyloucks394@gmail.com>
2025-03-30 16:30:48 +0000

    Add 20.java

---
+++ src/20.java
@@ -0,0 +1,6 @@
+public class ScienceExperiment {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is my project.");
+        System.out.println("Exploring the wonders of science.");
+    }
+}
