5c7ae0775211620a15f9a02e0a4fa7441ce95ec8
baneyloucks394 <baneyloucks394@gmail.com>
2025-03-31 12:49:04 +0000

    Add 21.java

---
+++ src/21.java
@@ -0,0 +1,14 @@
+public class ScienceExperiment {
+    public static void main(String[] args) {
+        // Example code to perform some scientific experiment
+        System.out.println("Hello, world!");
+        System.out.println("This is an example of a scientific experiment.");
+        
+        // Simulate some data processing
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            sum += i;
+        }
+        System.out.println("The total sum is: " + sum);
+    }
+}
