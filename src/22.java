fab7981a0cf1dfe8f969beadc7d2ee323ea04215
baneyloucks394 <baneyloucks394@gmail.com>
2025-04-01 08:02:07 +0000

    Add 22.java

---
+++ src/22.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ScienceExperiment {
+    public static void main(String[] args) {
+        List<String> items = new ArrayList<>();
+        items.add("Apple");
+        items.add("Banana");
+        items.add("Cherry");
+
+        for (String item : items) {
+            System.out.println(item);
+        }
+    }
+}
