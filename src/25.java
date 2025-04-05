f37f1b405e5083d5e64e43a4a05bb76ccf988531
baneyloucks394 <baneyloucks394@gmail.com>
2025-04-05 04:57:50 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,13 @@
+public class ExperimentCodeGenerator {
+    public static void main(String[] args) {
+        // Example code generation
+        System.out.println("Hello, world!");
+        double pi = Math.PI;
+        System.out.printf("%.4f", pi);
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        System.out.println("Sum: " + sum + ", Difference: " + difference);
+    }
+}
