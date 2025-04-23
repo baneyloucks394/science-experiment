f9a6c574467cc3a1c44676295e6f5c0f6facd4a8
baneyloucks394 <baneyloucks394@gmail.com>
2025-04-23 10:54:36 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,11 @@
+public class ScienceExperiment {
+    public void experiment1() {
+        // Code to execute for experiment 1
+        System.out.println("Executing experiment 1");
+    }
+
+    public void experiment2() {
+        // Code to execute for experiment 2
+        System.out.println("Executing experiment 2");
+    }
+}
