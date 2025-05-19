c2ebf092f4eacbb9d16b97dea18d0b1be5439cfb
baneyloucks394 <baneyloucks394@gmail.com>
2025-05-19 13:02:23 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class ScienceExperiment {
+    public static void main(String[] args) {
+        // Generate a random number between 1 and 10
+        int randomNumber = (int) (Math.random() * 9 + 1);
+        
+        // Print the result
+        System.out.println("Random number: " + randomNumber);
+    }
+}
