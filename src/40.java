7655593ec6a05ecce063fbc824458aca85a38463
baneyloucks394 <baneyloucks394@gmail.com>
2025-05-24 07:17:34 +0000

    Add 40.java

---
+++ src/40.java
@@ -0,0 +1,5 @@
+public class ScienceExperimentProject {
+    public static void main(String[] args) {
+        System.out.println("This is a sample Java code for an experiment in my project.");
+    }
+}
